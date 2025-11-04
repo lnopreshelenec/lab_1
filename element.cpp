1ca4e89a7177722da4fdf03e290d60c5cb01fddf
AFedorova <fedorov4.alisa@yandex.ru>
2025-11-04 21:38:34 +0300

    Перераспределено по h и cpp файлов

--- element.cpp
+++ element.cpp
@@ -1,3 +1,63 @@
-// #include "element.h"
+#include "element.h"
+template<class T>
+Element<T>::Element(){}
+
+template<class T>
+Element<T>::~Element(){
+    if(this){
+        if(this->next){
+            delete this->next;
+        }
+    }
+}
+
+template<class T>
+T Element<T>::get_data() const {
+    if(this){
+        return data;
+    }
+    return 0;
+}
+
+template<class T>
+Element<T>* Element<T>::get_next() const {
+
+    if(this){
+        return next;
+    }
+    return 0;
+}
 
 
+template<class T>
+void Element<T>::set_data(T val){
+
+    if(this){
+        data = val;
+    }
+}
+template<class T>
+void Element<T>::set_next(Element<T>* val){
+
+    if(this){
+        next = val;
+    }
+}
+
+
+template<class T>
+Element<T>* Element<T>::get_prev() const{
+    if(this){
+        return prev;
+    }
+    return 0;
+}
+
+
+template<class T>
+void Element<T>::set_prev(Element<T>* val){
+    if(this){
+        prev = val;
+    }
+}
+template class Element<Conf*>;
